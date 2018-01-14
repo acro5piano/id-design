61cf673e01d8ebb34887e506fc1795e847c801b6
gosho-kazuya <ketsume0211@gmail.com>
2018-01-14 23:42:17 +0900

    header fixed

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import logo from 'assets/logo.png';
+import invertedLogo from 'assets/logo-inverted.png';
 
 export default class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      scrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      this.setState({
+        scrolled: (window.pageYOffset > window.innerHeight / 2)
+      })
+    }, false);
+  }
+
   render() {
     return (
-      <div className="Header">
+      <div className={'Header' + (this.state.scrolled ? ' is-fixed' : '')}>
         <div className="Header-logo">
           <a href="/">
-            <img src={logo} className="Header-logo" alt="logo" />
+            <img
+              src={this.state.scrolled ? invertedLogo : logo }
+              className={'Header-logo' + (this.state.scrolled ? ' is-inverted' : '')}
+              alt="logo" />
           </a>
         </div>
         <div className="Header-nav">
