188f9821494fc16d987068dded95a7249436977d
gosho-kazuya <ketsume0211@gmail.com>
2018-02-04 14:40:48 +0900

    data outside

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,52 +1,12 @@
 import React, { Component } from 'react';
 
 import { setFadeinTransition } from 'common'
-
-import image1 from 'assets/top-1.png';
-import image2 from 'assets/top-2.png';
-import image3 from 'assets/top-3.png';
-import video from 'assets/sample.mp4';
-import logo from 'assets/logo.png';
-
-import gallayImage0 from 'assets/top-gallary-0.jpg';
-import gallayImage1 from 'assets/top-gallary-1.jpg';
-import gallayImage2 from 'assets/top-gallary-2.jpg';
-import gallayImage3 from 'assets/top-gallary-3.jpg';
-import gallayImage4 from 'assets/top-gallary-4.jpg';
-import gallayImage5 from 'assets/top-gallary-5.jpg';
-
-import marinImage from 'assets/top-marin.jpg';
-
-const gallaryImages = [
-  gallayImage0,
-  gallayImage1,
-  gallayImage2,
-  gallayImage3,
-  gallayImage4,
-  gallayImage5,
-];
+import { top } from 'data'
 
 export default class Top extends Component {
-  constructor () {
-    super()
-    this.state = {
-      slickImage: image1
-    }
-  }
 
   componentDidMount() {
     setFadeinTransition()
-    setInterval(() => {
-      this.setState({
-        slickImage: this._nextImage
-      })
-    }, 5000)
-  }
-
-  get _nextImage() {
-    if (this.state.slickImage.match('top-1')) return image2
-    if (this.state.slickImage.match('top-2')) return image3
-    if (this.state.slickImage.match('top-3')) return image1
   }
 
   render() {
@@ -61,12 +21,12 @@ export default class Top extends Component {
             </div>
             <div className="Top-movie-video-area">
               <video loop muted autoPlay className="Top-movie-video">
-                <source src={video} />
+                <source src={top.videos[0].src} />
               </video>
             </div>
           </div>
         </div>
-        <div className="Top-section">
+        <div className="Top-section fadein">
           <h2 className="Top-head">
             Asana’s mission is to help humanity thrive by enabling all teams to work together effortlessly.
           </h2>
@@ -76,8 +36,8 @@ export default class Top extends Component {
         </div>
         <div className="Top-section fadein">
           <div className="Top-gallary">
-            {gallaryImages.map((image, index) =>
-              <img className="Top-gallary-image" key={index} src={image} alt={'gallayImage' + index} />
+            {top.images.map((image, index) =>
+              <img className="Top-gallary-image" key={index} src={image.src} alt={image.alt} />
             )}
           </div>
         </div>
@@ -92,7 +52,7 @@ export default class Top extends Component {
               </p>
             </div>
             <div className="six columns">
-              <img className="fade u-full-width" src={marinImage} alt="marin" />
+              <img className="fade u-full-width" src="/images/top-marin.jpg" alt="marin" />
             </div>
           </div>
         </div>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,36 +2,9 @@ import React, { Component } from 'react';
 
 import { setFadeinTransition } from 'common'
 
-import image1 from 'assets/top-1.png';
-import image2 from 'assets/top-2.png';
-import image3 from 'assets/top-3.png';
-import video from 'assets/sample.mp4';
-
-import gallayImage0 from 'assets/top-gallary-0.jpg';
-import gallayImage1 from 'assets/top-gallary-1.jpg';
-import gallayImage2 from 'assets/top-gallary-2.jpg';
-import gallayImage3 from 'assets/top-gallary-3.jpg';
-import gallayImage4 from 'assets/top-gallary-4.jpg';
-import gallayImage5 from 'assets/top-gallary-5.jpg';
-
-import marinImage from 'assets/top-marin.jpg';
-
-const gallaryImages = [
-  gallayImage0,
-  gallayImage1,
-  gallayImage2,
-  gallayImage3,
-  gallayImage4,
-  gallayImage5,
-];
+import { works } from 'data'
 
 export default class Top extends Component {
-  constructor () {
-    super()
-    this.state = {
-      slickImage: image1
-    }
-  }
 
   componentDidMount() {
     setFadeinTransition()
@@ -47,27 +20,23 @@ export default class Top extends Component {
         </div>
         <div className="Works-section">
           <div className="Works-gallary">
-            {gallaryImages.map((image, index) =>
-              <div
-                className="Works-gallary-item fadein"
-                key={index}>
+            {works.map((work, index) =>
+              <div className="Works-gallary-item fadein" key={index}>
                 <div className="Works-gallary-hover-area">
                   <div className="Works-gallary-hover-date">
-                    30 November, 2017
+                    {work.date}
                   </div>
                   <div className="Works-gallary-hover-description">
-                    Aedas projects recognised for design excellence at A&D Trophy Awards 2017
+                    {work.description}
                   </div>
                 </div>
-                <img
-                  className="Works-gallary-image"
-                  src={image} alt={'gallayImage' + index} />
+                <img className="Works-gallary-image" src={work.thumbnailSrc} alt={work.description} />
                 <div className="Works-gallary-text">
                   <div className="Works-gallary-date">
-                    30 November, 2017
+                    {work.date}
                   </div>
-                  <div className="Works-gallary-desctiption">
-                    Aedas projects recognised for design excellence at A&D Trophy Awards 2017
+                  <div className="Works-gallary-description">
+                    {work.description}
                   </div>
                 </div>
               </div>
